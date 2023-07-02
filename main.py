f45ac0d4169d11077a17f5e40378cf6b70fa8661
Snilk <nilk.ao@gmail.com>
2023-07-02 15:04:47 -0400

    agregamos endpoint suma

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+#ruta Raiz
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
+#las rutas le dice poe, endpoit
+#ruta items y pide un parametro numerico, 
+# tambien se puede anexar un valor string que es opcional
+# ejemplo: http://127.0.0.1:8000/items/10?q=mouse
+# R. {"item_id":10,"q":"mouse"}
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+#ejemplo de una calculadora que sume
+@app.get("/suma/")
+def calculadora(opeardor1:float,operador2:float):
+    return {'suma':opeardor1+operador2}
